7ffb72c762b6e62e5311121165f74b34f6ebc059
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2022-08-10 22:37:22 +0700

    Implement routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Main from './pages/mainPage/Main'
+import Main from './pages/mainPage/Main';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
   return (
     <div className='App'>
-      <Main />
+      <Routes>
+        <Route path='/home' element={<Main />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='*' element={<Navigate to='/home' replace />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/mainPage/Main.js
+++ src/pages/mainPage/Main.js
@@ -1,26 +1,28 @@
 import './Main.css';
 // import '../../components/dashboard/background/DashboardBackground.css';
-import { VStack, Flex, Spacer} from '@chakra-ui/layout';
+import { VStack, Flex, Spacer } from '@chakra-ui/layout';
 import DashboardNavigationBar from '../../components/dashboard/navigationBar/DashboardNavigationBar';
 import SideBar from '../../components/dashboard/sideBar/SideBar';
 import Pricing from '../../components/pricing/Pricing';
 import FrontPage from '../../components/frontPage/FrontPage';
 import SignUpBox from '../../components/signUpBox/SignUpBox';
 import NavigationBar from '../../components/navigationBar/NavigationBar';
+import SignUpPage from '../SignUpPage';
+import LoginPage from '../LoginPage';
 
 const Main = () => {
   return (
     <div>
-      <div className="bg-image"></div>
+      <div className='bg-image'></div>
       <VStack w='full' h='full' spacing={10}>
-        <NavigationBar/>
+        <NavigationBar />
         <Spacer></Spacer>
         <Flex>
-          <SignUpBox/>
+          <FrontPage />
         </Flex>
       </VStack>
     </div>
   );
-}
+};
 
 export default Main;
