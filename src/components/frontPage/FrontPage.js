8a6fcea46afeedbbb06251adce86c1ef3123b694
energizedNachos <16521108@std.stei.itb.ac.id>
2022-08-12 11:52:16 +0700

    Update FrontPage.js

--- src/components/frontPage/FrontPage.js
+++ src/components/frontPage/FrontPage.js
@@ -1,11 +1,11 @@
-//Belom kelar. Gausah dicoba-coba dulu. Baru bgt dibuat//
-import { Text, HStack, Image, VStack, Button } from '@chakra-ui/react';
+import { Text, HStack, Image, VStack, Button, Link } from '@chakra-ui/react';
+import { NavLink } from 'react-router-dom';
+
 const FrontPage = () => {
   return (
     <HStack
       w='full'
       h='full'
-      alignItems='baseline'
       marginTop={'10%'}
       alignItems='center'
     >
@@ -29,7 +29,11 @@ const FrontPage = () => {
             </Text>
           </VStack>
         </VStack>
-        <Button bg='#49439B' color='white' fontWeight='bold' borderRadius={0} w='220px' h='40px' fontSize={18} _hover={{ bg: '#231E63' }}>DAFTAR SEKARANG</Button>
+        <NavLink to='/signup'>
+          <Link>
+          <Button bg='#49439B' color='white' fontWeight='bold' borderRadius={0} w='220px' h='40px' fontSize={18} _hover={{ bg: '#231E63' }}>DAFTAR SEKARANG</Button>
+          </Link>
+        </NavLink>
       </VStack>
       <Image src='./pie-chart.png' boxSize={500}/>
     </HStack>
