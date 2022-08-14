75f2679471fbbfce58026b8b8b4c54949b51fd66
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2022-08-14 19:12:35 +0700

    Fix help page styling and fix login routing

--- src/pages/homePages/LoginPage.js
+++ src/pages/homePages/LoginPage.js
@@ -1,10 +1,23 @@
-import MainWrapper from "./mainWrapper/MainWrapper";
+import MainWrapper from './mainWrapper/MainWrapper';
 import LoginBox from '../../components/loginBox/LoginBox';
+import { useEffect } from 'react';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        navigate('/dashboard', { replace: true });
+      }
+    });
+  }, []);
+
   return (
     <MainWrapper>
-      <LoginBox/>
+      <LoginBox />
     </MainWrapper>
   );
 };
