a086c2e353af3a4ce4b420c616c98b19e963b5cb
Jessica <jessicalistiaraa@gmail.com>
2022-08-15 02:50:02 +0700

    Update Filling Form

--- src/components/fillingform/Fillingform.js
+++ src/components/fillingform/Fillingform.js
@@ -1,98 +1,104 @@
-import {  Box, Text, Stack, Spacer, Button, Image,  Flex, Checkbox, Link, Heading,Input, Radio, Slider,
+import {  Box, Text, Stack, Spacer, Button, Image,  Flex, Checkbox, Link, HStack, VStack, Input, Radio, Slider,
     SliderTrack,
     SliderFilledTrack,
     SliderThumb,
     SliderMark,
- } from "@chakra-ui/react";
- import React from "react";
+} from "@chakra-ui/react";
+import React from "react";
+import FormalHeading from "../FormalHeading";
+import { PhoneIcon } from '@chakra-ui/icons';
+import "@fontsource/nunito";
+import "@fontsource/raleway";
+import "@fontsource/krona-one";
+
 
 const FillingForm = () => {
     const [sliderValue, setSliderValue] = React.useState(50)
     return (
-        <Flex
-            align="center"
+        <VStack
+            align="left"
             direction="column"
-            m="0 auto"
-            maxW={{ xl: "1200px" }}
         >   
-            <Box bg='#FFFFFF' borderRadius={'md'} bgOpacity="25%" p={4}>
-                <Flex>   
-                    <Heading pl={2}>
-                        Judul Survei
-                    </Heading>
-                    <Spacer></Spacer>
-                    <Link align="right" p={2}>
-                        Contact Surveyor
-                    </Link>
-                </Flex> 
-                <Box bg="#122543" borderRadius={'md'} p={2} m={2}>
-                    <Box p={2}>
-                        <Text color={'white'}>Pertanyaan</Text>
+            <FormalHeading title='JUDUL SURVEI'/>
+            <HStack alignSelf={'end'} pb={2}>
+                <PhoneIcon color='white' boxSize='6'/>
+                <Link align="right" p={2} color='white' fontFamily='Nunito' fontSize='20px'>
+                    Contact Surveyor
+                </Link>
+            </HStack>
+            <Box bg='#7991b2' borderRadius='20px' p={6} w='100%' h='full'>
+                <VStack align='left' spacing='25px' overflowY={'scroll'} maxHeight='22em' css={{"&::-webkit-scrollbar": {width:"8px",},"&::-webkit-scrollbar-track": {background:'#D9D9D9', borderRadius:"50px",},"&::-webkit-scrollbar-thumb": {background:'#122543', borderRadius:"50px",},}}>
+                    <Box bg="#122543" borderRadius='25px'>
+                        <Box pt={4} pb={3} pl={5} pr={5}>
+                            <Text color={'white'} fontFamily='Raleway' fontSize='20px'>Pertanyaan</Text>
+                        </Box>
+                        <Box pb={5} pl={5} pr={5}>
+                            <Image boxSize='100%' src="https://www.jcceramictiles.com/Content/upload/2017249249/201711241527057755410.jpg"></Image>
+                        </Box>
+                        <Box borderRadius='25px' bg='white' w='100%' h='full'>
+                            <Input placeholder='Basic usage' />
+                        </Box>
                     </Box>
-                    <Box p={2}>
-                        <Image src="https://www.jcceramictiles.com/Content/upload/2017249249/201711241527057755410.jpg"></Image>
+                    <Box bg="#122543" borderRadius='25px'>
+                        <Box pt={4} pb={3} pl={5} pr={5}>
+                            <Text color={'white'} fontFamily='Raleway' fontSize='20px'>Pertanyaan</Text>
+                        </Box>
+                        <Box bg='white' borderRadius='25px' p={2}>
+                            <Stack mt={1} spacing={1} pl={4} pr={4} pb={1} fontFamily='Raleway' color='black'>
+                                <Checkbox defaultChecked>Checkbox</Checkbox>
+                                <Checkbox defaultChecked>Checkbox</Checkbox>
+                                <Checkbox defaultChecked>Checkbox</Checkbox>
+                            </Stack>
+                        </Box>
                     </Box>
-                    <Box p={2} borderRadius={'md'} bg='white'>
-                        <Input placeholder='Basic usage' />
+                    <Box bg="#122543" borderRadius='25px'>
+                        <Box pt={4} pb={3} pl={5} pr={5}>
+                            <Text color={'white'} fontFamily='Raleway' fontSize='20px'>Pertanyaan</Text>
+                        </Box>
+                        <Box bg='white' borderRadius='25px' p={2}>
+                            <Stack mt={1} spacing={1} pl={4} pr={4} pb={1} fontFamily='Raleway' color='black'>
+                                <Radio value='1'>First</Radio>
+                                <Radio value='2'>Second</Radio>
+                                <Radio value='3'>Third</Radio>
+                            </Stack>
+                        </Box>
                     </Box>
-                </Box>
-                <Box bg="#122543" borderRadius={'md'} p={2} m={2}>
-                    <Box p={2}>
-                        <Text color={'white'}>Pertanyaan</Text>
+                    <Box bg="#122543" borderRadius='25px'>
+                        <Box pt={4} pb={3} pl={5} pr={5}>
+                            <Text color={'white'} fontFamily='Raleway' fontSize='20px'>Pertanyaan</Text>
+                        </Box>
+                        <Box bg='white' pl={14} pr={14} pt={6} pb={10} borderRadius='25px'>
+                            <Slider oncChange={(val)=> setSliderValue(val)}>
+                                <SliderMark value={0} color='black'>
+                                    0%
+                                </SliderMark>
+                                <SliderMark value={25} color='black'>
+                                    25%
+                                </SliderMark>
+                                <SliderMark value={50} color='black'>
+                                    50%
+                                </SliderMark>
+                                <SliderMark value={75} color='black'>
+                                    75%
+                                </SliderMark>
+                                <SliderMark value={100} color='black'>
+                                    100%
+                                </SliderMark>
+                                <SliderTrack>
+                                    <SliderFilledTrack />
+                                </SliderTrack>
+                                <SliderThumb/>
+                            </Slider>
+                        </Box>
                     </Box>
-                    <Box bg='white' borderRadius={'md'} p={2}>
-                        <Stack  mt={1} spacing={1}>
-                            <Checkbox defaultChecked>Checkbox</Checkbox>
-                            <Checkbox defaultChecked>Checkbox</Checkbox>
-                            <Checkbox defaultChecked>Checkbox</Checkbox>
-                        </Stack>
-                    </Box>
-                </Box>
-                <Box bg='#122543' borderRadius={'md'} p={2} m={2}>
-                    <Box p={2}>
-                        <Text color='white'>Pertanyaan</Text>
-                    </Box>
-                    <Box bg='white' borderRadius={'md'} p={2}>
-                        <Stack mt={1} spacing={1}>
-                            <Radio value='1'>First</Radio>
-                            <Radio value='2'>Second</Radio>
-                            <Radio value='3'>Third</Radio>
-                        </Stack>
-                    </Box>
-                </Box>
-                <Box bg='#122543' borderRadius={'md'} p={2} m={2}>
-                    <Box p={2}>
-                        <Text color='white'>Pertanyaan</Text>
-                    </Box>
-                    <Box bg='white' p={2} borderRadius={'md'}>
-                        <Slider oncChange={(val)=> setSliderValue(val)}>
-                            <SliderMark value={0}>
-                                0%
-                            </SliderMark>
-                            <SliderMark value={25}>
-                                25%
-                            </SliderMark>
-                            <SliderMark value={50}>
-                                50%
-                            </SliderMark>
-                            <SliderMark value={75}>
-                                75%
-                            </SliderMark>
-                            <SliderMark value={100}>
-                                100%
-                            </SliderMark>
-                            <SliderTrack>
-                                <SliderFilledTrack />
-                            </SliderTrack>
-                            <SliderThumb />
-                        </Slider>
-                    </Box>
-                </Box>
-                <Box align="right" pr={2}>
-                    <Button bg='#EA8238'>Submit</Button>
-                </Box>
+                </VStack>
             </Box>
-        </Flex>
+            <Box align='right' pt={6}>
+                <Button color='black' bgColor='#EA8238' _hover={{ bg: '#d66a1e' }} w='180px' h='55px' fontSize='18px' fontFamily='Krona One' borderRadius='27px' borderColor='black' border='1px'>
+                    SUBMIT
+                </Button>
+            </Box>
+        </VStack>
     );
 }
 
