2d7e8bd935f3b6805e7665d21bd3d89dd859b8e0
HoseaNA <110534062+HoseaNA@users.noreply.github.com>
2022-08-14 15:15:16 +0700

    Check box

--- src/components/ToS/ToS.js
+++ src/components/ToS/ToS.js
@@ -18,8 +18,7 @@ const ToS = () => {
                     <Spacer></Spacer>
                     <Text>Etiam ac consectetur felis, pretium vehicula urna. Sed mollis congue quam sit amet porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in egestas purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque ultrices viverra leo eget pulvinar. Fusce malesuada scelerisque nisl, ut viverra mi convallis vitae. Vivamus nec quam sed mi sollicitudin faucibus quis sed arcu. Aliquam in bibendum massa, quis rutrum diam. Duis et rhoncus mi, id viverra mauris. Nunc gravida malesuada odio, non fermentum massa porta eget. Donec lorem lectus, maximus at sollicitudin at, suscipit dictum quam. Donec porttitor nisl libero, cursus volutpat purus consequat eu. Nullam sed iaculis ante. Pellentesque urna sapien, vehicula non elit quis, accumsan fringilla metus. Phasellus odio ipsum, tincidunt id fringilla sed, condimentum vel magna.</Text>
                     <HStack>
-                        <Checkbox colorScheme='black'></Checkbox>
-                        <Text>I agree to the</Text>
+                        <Checkbox colorScheme='blue'>I agree to the</Checkbox>
                         <Link color='blue' href='#'>terms of service</Link> 
                         {/* tinggal hrefnya diganti */}
                     </HStack>
