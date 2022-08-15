c4230740d9c2a17300999f0e679f03859b55fc60
wildhan_hdy <90306668+WildhanH@users.noreply.github.com>
2022-08-15 12:34:45 +0700

    Update AnalysisPage.js

--- src/components/analysisPage/AnalysisPage.js
+++ src/components/analysisPage/AnalysisPage.js
@@ -29,14 +29,14 @@ const AnalysisPage = () => {
                         </Box>
                     </Box>
                     <Box w="100%" bg="#0F2543" boxShadow="dark-lg" borderRadius={20}>
-                        <Text pl="1.5%" py="1%" color="white" fontFamily="raleway" fontSize="3xl" align="left">Kisaran Uang yang Rela Dikeluarkan Pembuat Survey untuk Mencapai Target Jumlah Responden</Text>
+                        <Text pl="1.5%" py="1%" color="white" fontFamily="raleway" fontSize="3xl" align="left">Kisaran Uang yang Rela Dikeluarkan Pembuat Survei untuk Mencapai Target Jumlah Responden</Text>
                         <Box w="100%" bg="white" borderRadius={20}>
                             <HStack w="full" h="full">
                                 <Image pl="2%" py="1%" boxSize="100%" w="40%" src="barchart_analysis.png"/>
                                 <Box pl="1%" pr="2%" fontFamily="nunito" fontSize="xl" align="left">
-                                    <Text as="span" color="black">Berdasarkan data survey, rata-rata persentase kategori budget adalah pilihan 3, yaitu budget dengan</Text>
+                                    <Text as="span" color="black">Berdasarkan data survei, rata-rata persentase kategori budget adalah pilihan 3, yaitu budget dengan</Text>
                                     <Text as="span" color="#4685B2"> kisaran Rp10.000 - Rp25.000</Text>
-                                    <Text as="span" color="black">. Kesimpulan yang dapat diambil adalah jumlah uang yang rela dikeluarkan oleh pembuat survei untuk mencapai target jumlah responden berada di </Text>
+                                    <Text as="span" color="black">. Kesimpulan yang dapat diambil adalah jumlah uang yang rela dikeluarkan oleh pembuat survei untuk mencapai target responden berada di </Text>
                                     <Text as="span" color="#4685B2"> kisaran Rp10.000 - Rp25.000</Text>
                                     <Text as="span" color="black">.</Text>
                                 </Box>
