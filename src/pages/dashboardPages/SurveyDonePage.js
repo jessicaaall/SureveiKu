c679e948645abeebb1de7951894632658ad6b851
energizedNachos <88883885+energizedNachos@users.noreply.github.com>
2022-08-15 21:14:01 +0700

    Update SurveyDonePage.js

--- src/pages/dashboardPages/SurveyDonePage.js
+++ src/pages/dashboardPages/SurveyDonePage.js
@@ -1,5 +1,5 @@
 import DashboardWrapper from './dashboardWrapper/DashboardWrapper';
-import SurveyDone from '../../components/surveydone/surveydone';
+import SurveyDone from '../../components/surveyDone/SurveyDone';
 
 const SurveyDonePage = () => {
   return (
