974689cab1b4692d374cec7fcc5496467acdcba0
Jessica <jessicalistiaraa@gmail.com>
2022-08-15 11:36:15 +0700

    Update teks Analysis Page

--- src/components/analysisPage/AnalysisPage.js
+++ src/components/analysisPage/AnalysisPage.js
@@ -17,11 +17,15 @@ const AnalysisPage = () => {
                     </Text>
                     <Box w='100%' h='auto' bg={'white'} color='black' borderRadius='25px' pl={'15px'}>
                         <HStack verticalAlign={'center'}>
-                            <Image src='./saysno.png' boxSize='20em'/>
-                            <Box pr={20}>
-                                <Text color='black' fontFamily='Nunito' fontSize='20px'>
-                                    Berdasarkan data tersebut dapat dilihat bahwa mahasiswa yang dapat mencapai target responden survei online mereka kurang dari 50%, yaitu hanya sekitar 35,2%. Hal ini menunjukkan bahwa adanya kesulitan yang cukup berarti yang dihadapi oleh para mahasiswa dalam mencapai target responden yang mereka perlukan. Oleh karena itu, perlu adanya inovasi yang dapat mengatasi kesulitan tersebut, mengingat jumlah responden merupakan salah satu hal yang sangat penting dalam mengumpulkan suatu data agar dapat dikatakan sebagai data yang valid.
-                                </Text>
+                            <Image src='./saysno.png' boxSize='25em'/>
+                            <Box pr={20} fontFamily='Nunito' fontSize='20px'>
+                                <Text as='span' color='black'>Berdasarkan data tersebut dapat dilihat bahwa mahasiswa yang dapat mencapai target responden survei online mereka</Text>
+                                <Text as='span' color='#AB2B2B'> kurang dari 50%</Text>
+                                <Text as='span' color='black'>, yaitu hanya sekitar</Text>
+                                <Text as='span' color='#EA8238'> 35,2%</Text>
+                                <Text as='span' color='black'>. Hal ini menunjukkan bahwa adanya kesulitan yang cukup berarti yang dihadapi oleh para mahasiswa dalam mencapai target responden yang mereka perlukan. Oleh karena itu,</Text>
+                                <Text as='span' color='#4685B2'>  perlu adanya inovasi yang dapat mengatasi kesulitan tersebut</Text>
+                                <Text as='span' color='black'> , mengingat jumlah responden merupakan salah satu hal yang sangat penting dalam mengumpulkan suatu data agar dapat dikatakan sebagai data yang valid.</Text>
                             </Box>
                         </HStack>
                     </Box>
@@ -34,8 +38,11 @@ const AnalysisPage = () => {
                         <HStack>
                             <Image src='./kisaranuangdiagram.png' h='30em' pl={10} pt={20} pb={10}/>
                             <Box pr={20}>
-                                <Text color='black' fontFamily='Nunito' fontSize='20px'>
+                                <Text as='span' color='black' fontFamily='Nunito' fontSize='20px'>
                                     Berdasarkan data survey, rata-rata persentasenya lihat kolom kategori_budget adalah pilihan 3, yaitu budget dengan kisaran Rp10.000 - Rp25.000. Kesimpulan yang dapat diambil adalah jumlah uang yang rela dikeluarkan oleh pembuat survey untuk mencapai target jumlah responden berada di kisaran Rp10.000 - Rp25.000.
+                                </Text>
+                                <Text as='span' color='orange'>
+                                    Halo
                                 </Text>
                             </Box>
                         </HStack>
