5a47a2a309ee98686352ed58c94c58593285cea9
energizedNachos <88883885+energizedNachos@users.noreply.github.com>
2022-08-15 21:35:31 +0700

    Update Redeem.js

--- src/components/redeemPoints/Redeem.js
+++ src/components/redeemPoints/Redeem.js
@@ -5,41 +5,40 @@ import {
     Button,
     VStack,
     HStack,
-    Flex
+    Flex,
+    Spacer
   } from '@chakra-ui/react';
 import FormalHeading from '../FormalHeading';
   
   const RedeemPoint = () => {
     function RewardBox(){
       return(
-        <Box w='300px' h='550px' bg='white' boxShadow='0 4px 12px 0 black' borderRadius={20} p={4}>
-          <VStack>
-            <Image src='./amazon-gift-card.jpeg' w={'15em'} pt={3} pb={4}/>
-            <Box pb={6}>
-              <Text fontSize='1.5em' fontWeight='bold' color='black' align='center' pb={2}>
+        <Box w='300px' h='65vh' bg='white' boxShadow='0 4px 12px 0 black' borderRadius={20} p={4}>
+          <Flex flexDirection='column' display='flex' alignItems='center'>
+            <Image src='./amazon-gift-card.jpeg' w='90%' pt={3} pb={4}/>
+              <Text fontSize='1.5em' fontWeight='bold' color='black' pb={2}>
                 Amazon $5 Giftcard
               </Text>
-              <Text align='left' color='black' fontSize='16px'>
-                What is Amazon Pay Gift Card? Amazon Pay Gift Cards are pre-paid
-                gift instruments that can be redeemed for purchasing items on Amazon.in
-                and it's partner merchants.
-              </Text>
-            </Box>
-            <Box align='center' color='black' pb={2}>
-              <Text>Stocks: 9.999</Text>
-              <Text>Points: 1.000</Text>
-            </Box>
-            <Box>
-              <Button bgColor='#EA8238' color='white' _hover={{ bg: '#d66a1e' }} w='8em' fontWeight='bold' borderRadius={20}>
-                REDEEM
-              </Button>
-            </Box>
-          </VStack>
+              <Text color='black' align='center' style={{whiteSpace: 'pre-wrap', overflowWrap: 'break-word'}}>
+                What is Amazon Pay Gift Card?
+                What is Amazon Pay Gift Card?
+                What is Amazon Pay Gift Card?
+                What is Amazon Pay Gift Card?
+                What is Amazon Pay Gift Card?
+                </Text>
+                <Box align='center' color='black' pb={2} pt={20}>
+                  <Text>Stocks: 9.999</Text>
+                  <Text>Points: 1.000</Text>
+                </Box>
+                  <Button bgColor='#EA8238' color='white' _hover={{ bg: '#d66a1e' }} w='8em' fontWeight='bold' borderRadius={20}>
+                    REDEEM
+                  </Button>
+          </Flex>
         </Box>
       )
     }
     return (
-      <VStack align='left' w='100%'>
+      <VStack align='left' w='83.5%' h='100vh' pos='fixed'>
         <FormalHeading title='REDEEM POINTS'></FormalHeading>
         <Text fontFamily='Raleway' color='white' fontSize='35px' fontWeigt='bold'>
           Your points : 17.000
@@ -55,7 +54,6 @@ import FormalHeading from '../FormalHeading';
           </HStack>
         </Box>
       </VStack>
-
     );
   };
   
