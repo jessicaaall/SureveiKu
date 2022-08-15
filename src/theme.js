c6c3117253a72dea466067980deb3693783749e8
energizedNachos <88883885+energizedNachos@users.noreply.github.com>
2022-08-15 17:24:33 +0700

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,9 @@
 import { extendTheme } from '@chakra-ui/react'
+import "@fontsource/poppins";
 
 const theme = extendTheme({
   fonts: {
-    heading: `'Poppins'`,
+    heading: `'Poppins', sans-serif`,
     body: `'Raleway', sans-serif`,
   },
 })
